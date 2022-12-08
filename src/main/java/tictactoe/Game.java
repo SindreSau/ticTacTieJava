ef0e200e45aa51d7419b52a1d9097a698f435a73
SindreSau <sindre.sauarlia@outlook.com>
2022-12-08 09:13:34 +0100

    TicTacToe v1.0.0

--- src/main/java/tictactoe/Game.java
+++ src/main/java/tictactoe/Game.java
@@ -1,6 +1,6 @@
 package tictactoe;
 
-import java.util.Arrays;
+import java.util.Objects;
 
 public class Game {
     static String[][] board =
@@ -8,10 +8,63 @@ public class Game {
              {"-", "-", "-"},
              {"-", "-", "-"}};
     static int turnCount = 0;
-    static boolean isCorrectFormat;
-    static String turn;
-    static String winner = "X";
-    static int[] ints = {0,0};
+    static boolean correctFormat = false;
+    static boolean correctPlacement = false;
+    static String turn = "X";
+    static boolean draw = false;
+    static String winner = null;
+    
+    
+    static String capitalize(String input) {
+        return input.toUpperCase();
+    }
+    
+    static boolean checkFormat(String userInput) {
+        char[] chArray = userInput.toCharArray();
+        
+        if (chArray.length == 2 && chArray[0] >= 65 && chArray[0] <= 67 && chArray[1] >= 49 && chArray[1] <= 51) {
+            correctFormat = true;
+        }
+        
+        if (!correctFormat) {
+            System.out.println("Sorry, wrong format. Try again!");
+        }
+        
+        return correctFormat;
+    }
+    
+    static boolean checkPlacement(String userInput) {
+        switch (userInput) {
+            case "A1" -> correctPlacement = !Objects.equals(board[0][0], "X") && !Objects.equals(board[0][0], "O");
+            case "A2" -> correctPlacement = !Objects.equals(board[0][1], "X") && !Objects.equals(board[0][1], "O");
+            case "A3" -> correctPlacement = !Objects.equals(board[0][2], "X") && !Objects.equals(board[0][2], "O");
+            case "B1" -> correctPlacement = !Objects.equals(board[1][0], "X") && !Objects.equals(board[1][0], "O");
+            case "B2" -> correctPlacement = !Objects.equals(board[1][1], "X") && !Objects.equals(board[1][1], "O");
+            case "B3" -> correctPlacement = !Objects.equals(board[1][2], "X") && !Objects.equals(board[1][2], "O");
+            case "C1" -> correctPlacement = !Objects.equals(board[2][0], "X") && !Objects.equals(board[2][0], "O");
+            case "C2" -> correctPlacement = !Objects.equals(board[2][1], "X") && !Objects.equals(board[2][1], "O");
+            case "C3" -> correctPlacement = !Objects.equals(board[2][2], "X") && !Objects.equals(board[2][2], "O");
+        }
+        
+        if (!correctPlacement) {
+            System.out.println("Sorry, wrong placement. Try again!");
+        }
+        return correctPlacement;
+    }
+    
+    static void updateBoard(String userInput) {
+        switch (userInput) {
+            case "A1" -> board[0][0] = turn;
+            case "A2" -> board[0][1] = turn;
+            case "A3" -> board[0][2] = turn;
+            case "B1" -> board[1][0] = turn;
+            case "B2" -> board[1][1] = turn;
+            case "B3" -> board[1][2] = turn;
+            case "C1" -> board[2][0] = turn;
+            case "C2" -> board[2][1] = turn;
+            case "C3" -> board[2][2] = turn;
+        }
+    }
     
     static void printBoard() {
         String[] letters = {"A ", "B ", "C "};
@@ -23,88 +76,65 @@ public class Game {
             }
             System.out.println();
         }
-        
-        if (turnCount % 2 == 0) {
-            turn = "X";
-        } else {
-            turn = "O";
-        }
-        
-        System.out.println("\n" + turn + "'s turn:");
-    }
     
-    static void incTurncount() {
-        if (isCorrectFormat) {
+        if (checkWinner()) {
+            System.out.println("");
+        } else if (correctFormat) {
             turnCount++;
-        }
-    }
-    
-    static boolean isCorrectInput(String userInput) {
-        userInput = userInput.toUpperCase();
-        char[] chArray = userInput.toCharArray();
-        
-        if (isCorrectPlace() && chArray.length == 2 && chArray[0] >= 65 && chArray[0] <= 67 && chArray[1] >= 49 && chArray[1] <= 51) {
-            isCorrectFormat = true;
-            return true;
+            
+            if (turnCount % 2 == 0) {
+                turn = "X";
+            } else {
+                turn = "O";
+            }
+            System.out.println("\n" + turn + "'s turn:");
         } else {
-            isCorrectFormat = false;
-            return false;
+            System.out.println("X will start:");
         }
-    }
-    
-    static boolean isCorrectPlace() {
-        boolean correctPlacement = false;
-        
-        return correctPlacement;
-    }
-    
-    static void setPiece(String userInput) {
-        userInput = userInput.toUpperCase();
-        switch (userInput) {
-            case "A1":
-                board[0][0] = turn;
-                ints = new int[]{0, 0};
-                break;
-            case "A2":
-                board[0][1] = turn;
-                ints = new int[]{0, 1};
-                break;
-            case "A3":
-                board[0][2] = turn;
-                break;
-            case "B1":
-                board[1][0] = turn;
-                break;
-            case "B2":
-                board[1][1] = turn;
-                ints = new int[]{1, 1};
-                break;
-            case "B3":
-                board[1][2] = turn;
-                break;
-            case "C1":
-                board[2][0] = turn;
-                break;
-            case "C2":
-                board[2][1] = turn;
-                break;
-            case "C3":
-                board[2][2] = turn;
-                break;
-        }
-    }
-    
-    static void getInts() {
-        System.out.println(Arrays.toString(ints));
+        //Initialize checkers:
+        correctFormat = false;
+        correctPlacement = false;
     }
     
     static boolean checkWinner() {
+        boolean hasWinner = false;
+        String[] winningLines = {
+                board[0][0] + board[0][1] + board[0][2],
+                board[1][0] + board[1][1] + board[1][2],
+                board[2][0] + board[2][1] + board[2][2],
+                board[0][0] + board[1][0] + board[2][0],
+                board[0][1] + board[1][1] + board[2][1],
+                board[0][2] + board[1][2] + board[2][2],
+                board[0][0] + board[1][1] + board[2][2],
+                board[0][2] + board[1][1] + board[2][0]
+        };
+    
+        for (String winningLine : winningLines) {
+            if (winningLine.equals("XXX")) {
+                winner = "X";
+                hasWinner = true;
+                break;
+            } else if (winningLine.equals("OOO")) {
+                winner = "O";
+                hasWinner = true;
+                break;
+            }
+        }
         
-        return false;
+        if (turnCount > 7) {
+            hasWinner = true;
+            draw = true;
+        }
+        
+        return hasWinner;
     }
     
-    static String winner() {
+    static void printWinner() {
         //Return X eller O
-        return winner;
+        if (draw) {
+            System.out.println("\nIt's a draw!");
+        } else {
+            System.out.println("\n" + winner + " has won the game!");
+        }
     }
 }
--- src/main/java/tictactoe/Main.java
+++ src/main/java/tictactoe/Main.java
@@ -7,27 +7,29 @@ public class Main {
     
     public static void main(String[] args){
         System.out.println("Welcome to TicTacToe.\nTo begin, enter a coordinate corresponding the game board. As an example, B2 would be the center.");
-        getInts();
         printBoard();
         Scanner s = new Scanner(System.in);
     
-        String in = null;
-        while (true) { //will be set to winner-boolean
-            try {
-                in = s.nextLine();
-                if (isCorrectInput(in)) {
-                    setPiece(in);
-                } else {
-                    System.out.println("Wrong format or placement, try again:");
-                    continue;
-                }
-            } catch (Exception e) {
-                System.out.println("Exception??");
+        String in;
+        while (winner == null) {
+            in = s.nextLine();
+            in = capitalize(in);
+            
+            if (!checkFormat(in)) {
                 continue;
             }
-            incTurncount();
-            printBoard();
+            boolean format = checkFormat(in);
+            boolean placement = checkPlacement(in);
+            
+            if (format && placement) {
+                updateBoard(in);
+                if (checkWinner()) {
+                    printWinner();
+                }
+                printBoard();
+            }
         }
-        /*s.close();*/
+        
+        s.close(); //Good practise to prevent data-leaks
     }
 }
